6c6ab5a5967fc58622c43170cb7c30df79109178
maneeshask <maneesha.sk2000@gmail.com>
2024-04-16 07:04:27 +0000

    COMPLETE ANSWER

--- app.js
+++ app.js
@@ -58,11 +58,68 @@ app.get('/states/:stateId/', async (request, response) => {
 
 app.post('/districts/', async (request, response) => {
   const districtDetails = request.body
-  const {district_id, district_name, state_id, cases, cured, active, deaths} =
-    districtDetails
-  const newDistrictQ = `INSERT INTO district (district_id, district_name, state_id, cases, cured, active, deaths) 
-                      VALUES (${district_id}, '${district_name}', ${state_id}, ${cases}, ${cured}, ${active}, ${deaths});
+  const {districtName, stateId, cases, cured, active, deaths} = districtDetails
+  const newDistrictQ = `INSERT INTO district (district_name, state_id, cases, cured, active, deaths) 
+                      VALUES ('${districtName}', ${stateId}, ${cases}, ${cured}, ${active}, ${deaths});
                       `
   const result = await db.run(newDistrictQ)
   response.send('District Successfully Added')
 })
+
+//API TO GET THE DISTRICT BASED ON THE ID
+
+app.get('/districts/:districtId/', async (request, response) => {
+  const {districtId} = request.params
+  const getDistrictQ = `SELECT * FROM district 
+  WHERE district_id=${districtId};`
+  const result = await db.get(getDistrictQ)
+  const resArr = convertDbobjIntoDistrictobj(result)
+  response.send(resArr)
+})
+
+//API TO DELETE A DISTRICT BASED ON THE GIVEN ID
+
+app.delete('/districts/:districtId/', async (request, response) => {
+  const {districtId} = request.params
+  const delDistrictQ = `DELETE FROM district 
+  WHERE district_id=${districtId};`
+  const result = await db.run(delDistrictQ)
+  response.send('District Removed')
+})
+
+//API TO UPDATE DETAILS OF GIVEN DISTRICT ID
+app.put('/districts/:districtId/', async (request, response) => {
+  const {districtId} = request.params
+  const districtDetails = request.body
+  const {districtName, stateId, cases, cured, active, deaths} = districtDetails
+  const updateDistrictQ = `UPDATE district 
+  SET district_name='${districtName}',
+  state_id=${stateId},cases=${cases},cured=${cured},active=${active},deaths=${deaths} 
+  WHERE district_id=${districtId};`
+  const result = await db.run(updateDistrictQ)
+  response.send('District Details Updated')
+})
+
+//API TO GET THE STATISTICS OF GIVEN STATE
+
+app.get('/states/:stateId/stats/', async (request, response) => {
+  const {stateId} = request.params
+  const stateStatisticsQ = `SELECT SUM(cases) AS totalCases,
+  SUM(cured) AS totalCured, SUM(active) AS totalActive, SUM(deaths) AS totalDeaths 
+  FROM district WHERE state_id=${stateId};`
+  const result = await db.get(stateStatisticsQ)
+  response.send(result)
+})
+
+//API TO GET THE STATENAME BASED ON GIVEN DISTRICT ID
+
+app.get('/districts/:districtId/details/', async (request, response) => {
+  const {districtId} = request.params
+  const getStateNameQ = `SELECT state_name AS stateName 
+  FROM district INNER JOIN state ON district.state_id=state.state_id 
+  WHERE district_id=${districtId};`
+  const result = await db.get(getStateNameQ)
+  response.send(result)
+})
+
+module.exports = app
